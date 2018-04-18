6d1f5545acaaa55fef8fe19c8d85730fbc12f50a
Valentin Brandl <vbrandl@riseup.net>
2018-04-18 19:24:14 +0200

    Pin compiler version

--- docker_build_musl.sh
+++ docker_build_musl.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-docker run -v $PWD:/volume -t clux/muslrust:nightly cargo build --release
+docker run -v $PWD:/volume -t clux/muslrust:1.25.0-nightly-2018-02-14 cargo build --release
+# docker run -v $PWD:/volume -t clux/muslrust:1.25.0-nightly-2018-02-14 nightly cargo build --release
