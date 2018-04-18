5c057f865f272bc04fb0acd9429d2993151c7469
Valentin Brandl <vbrandl@riseup.net>
2018-04-18 19:44:30 +0200

    Pin compiler version

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM rustlang/rust:nightly
+
+RUN rustup install nightly-2018-02-14
--- docker_build_glibc.sh
+++ docker_build_glibc.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-docker run -v $PWD:/usr/src/build -t rustlang/rust:nightly bash -c "cd /usr/src/build && cargo build --release"
+CONTAINER_NAME="custom/rustnightly:nightly-2018-02-14"
+
+[ ! "$(docker ps -a | grep $CONTAINER_NAME)" ] && docker build -t $CONTAINER_NAME .
+
+docker run -v $PWD:/usr/src/build -t $CONTAINER_NAME bash -c "cd /usr/src/build && cargo build --release"
